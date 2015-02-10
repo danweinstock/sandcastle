9a8268b8e97f4f2e4c6c232d76e1169f510d34c2
danweinstock <dan.weinstock@yahoo.com>
2015-02-10 16:16:32 -0500

    Price in dollars

--- sandcastle/db/seeds.rb
+++ sandcastle/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(
+	:name => "The Martian by Andy Weir",
+	:description => "Excellent book. A must read!",
+	:price_in_cents => 3999
+	)
+Product.create!(
+	:name => "V for Vendetta",
+	:description => "Great movie. Inspired by graphic novel of the same name.",
+	:price_in_cents => 799
+	)

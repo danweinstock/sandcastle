3b7de9ef4548249ec6f02a437856a2feb78c3770
danweinstock <dan.weinstock@yahoo.com>
2015-02-10 17:21:45 -0500

    Writing review routes, controllers and views

--- sandcastle/config/routes.rb
+++ sandcastle/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :products
+  resources :products do
+  	resources :reviwes, only: [:show, :create, :destroy]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   
